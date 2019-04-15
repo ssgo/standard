324c9cfee0eb9cedd1f0d6736b062b5fd2adf328
Star <star3s@126.com>
2019-04-15 14:35:47 +0800

    Add DiscoverHeaderScheme X-Scheme

--- Log.go
+++ Log.go
@@ -5,8 +5,8 @@ const LogLevelInfo = "info"
 const LogLevelWarning = "warning"
 const LogLevelError = "error"
 
-const LogFieldType = "logType"                  // 日志类型
-const LogFieldTime = "logTime"                  // 日志时间，格式为float64，单位秒
-const LogFieldLevel = "logLevel"                // 日志级别
-const LogFieldTraces = "traces"                 // 调用跟踪，以 "; " 间隔的字符串
-const LogTypeUndefined = "undefined"            // 不符合规则的日志
+const LogFieldType = "logType"       // 日志类型
+const LogFieldTime = "logTime"       // 日志时间，格式为float64，单位秒
+const LogFieldLevel = "logLevel"     // 日志级别
+const LogFieldTraces = "traces"      // 调用跟踪，以 "; " 间隔的字符串
+const LogTypeUndefined = "undefined" // 不符合规则的日志
