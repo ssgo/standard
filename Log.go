1a1a68bd6c0979b983f80eef501d37e93ea93fd9
Star <star3s@126.com>
2020-04-17 21:44:05 +0800

    Fix log to es mapping illegal

--- Log.go
+++ Log.go
@@ -57,11 +57,11 @@ type ErrorLog struct {
 
 type DBLog struct {
 	BaseLog
-	DbType   string        // 数据库类型，例如：mysql、oracle、redis...
-	Dsn      string        // 连接信息
-	Query    string        // 请求内容
-	Args     []interface{} // 请求参数，会变化的部分应该记录在此
-	UsedTime float32       // 处理请求花费的时间，格式为float32，单位毫秒
+	DbType    string  // 数据库类型，例如：mysql、oracle、redis...
+	Dsn       string  // 连接信息
+	Query     string  // 请求内容
+	QueryArgs string  // 请求参数，会变化的部分应该记录在此
+	UsedTime  float32 // 处理请求花费的时间，格式为float32，单位毫秒
 }
 
 type DBErrorLog struct {
@@ -89,13 +89,13 @@ type ServerErrorLog struct {
 
 type TaskLog struct {
 	BaseLog
-	Name      string  // 任务名称
-	Args      string  // 任务参数
-	Succeed   bool    // 是否成功
-	Node      string  // 运行在哪个节点（ip:port）
-	StartTime string  // 日志时间，格式为 RFC3339Nano "2006-01-02T15:04:05.999999999Z07:00"
-	UsedTime  float32 // 处理任务花费的时间，格式为float32，单位毫秒
-	Memo      string  // 备注
+	Name      string                 // 任务名称
+	Args      map[string]interface{} // 任务参数
+	Succeed   bool                   // 是否成功
+	Node      string                 // 运行在哪个节点（ip:port）
+	StartTime string                 // 日志时间，格式为 RFC3339Nano "2006-01-02T15:04:05.999999999Z07:00"
+	UsedTime  float32                // 处理任务花费的时间，格式为float32，单位毫秒
+	Memo      string                 // 备注
 }
 
 type MonitorLog struct {
@@ -126,27 +126,27 @@ type StatisticLog struct {
 
 type RequestLog struct {
 	BaseLog
-	ServerId           string            // 服务编号（用于跟踪哪一个服务）
-	App                string            // 应用名
-	Node               string            // 处理请求的节点，ip:port
-	ClientIp           string            // 真实的用户IP，通过 X-Real-IP 续传
-	FromApp            string            // 调用方应用
-	FromNode           string            // 调用方节点，格式 ip:port
-	ClientId           string            // 客户唯一编号，通过 X-Client-ID 续传
-	SessionId          string            // 会话唯一编号，通过 X-Session-ID 续传
-	RequestId          string            // 请求唯一编号，通过 X-Request-ID 续传
-	Host               string            // 真实用户请求的Host，通过 X-Host 续传
-	Scheme             string            // http scheme, http or https
-	Proto              string            // http proto, 1.1 or 2.0
-	AuthLevel          int               // 验证级别，用来校验用户是否有权限访问
-	Priority           int               // 优先级，用来在服务故障时进行自动降级处理
-	Method             string            // 请求的方法
-	Path               string            // 请求的路径，不包括GET参数部分，如果有PATH参数应该记录定义的PATH
-	RequestHeaders     map[string]string // 请求头，排除掉指定不需要信息后的所有头部内容，敏感数据应脱敏
+	ServerId           string                 // 服务编号（用于跟踪哪一个服务）
+	App                string                 // 应用名
+	Node               string                 // 处理请求的节点，ip:port
+	ClientIp           string                 // 真实的用户IP，通过 X-Real-IP 续传
+	FromApp            string                 // 调用方应用
+	FromNode           string                 // 调用方节点，格式 ip:port
+	ClientId           string                 // 客户唯一编号，通过 X-Client-ID 续传
+	SessionId          string                 // 会话唯一编号，通过 X-Session-ID 续传
+	RequestId          string                 // 请求唯一编号，通过 X-Request-ID 续传
+	Host               string                 // 真实用户请求的Host，通过 X-Host 续传
+	Scheme             string                 // http scheme, http or https
+	Proto              string                 // http proto, 1.1 or 2.0
+	AuthLevel          int                    // 验证级别，用来校验用户是否有权限访问
+	Priority           int                    // 优先级，用来在服务故障时进行自动降级处理
+	Method             string                 // 请求的方法
+	Path               string                 // 请求的路径，不包括GET参数部分，如果有PATH参数应该记录定义的PATH
+	RequestHeaders     map[string]string      // 请求头，排除掉指定不需要信息后的所有头部内容，敏感数据应脱敏
 	RequestData        map[string]interface{} // 请求的数据内容，JSON对象，集合类型仅记录少量内容，敏感数据应脱敏，非对象内容过大应做截取
-	UsedTime           float32           // 处理请求花费的时间，格式为float32，单位毫秒
-	ResponseCode       int               // 应答代码，200 1000+ 正常应答，201～399，1～199  600～999 特殊应答，<1 异常应答
-	ResponseHeaders    map[string]string // 应答头，排除掉指定不需要信息后的所有头部内容，敏感数据应脱敏
-	ResponseDataLength uint              // 应答的数据长度
-	ResponseData       string            // 指定要记录的数据内容，JSON对象，集合类型仅记录少量内容，敏感数据应脱敏，非对象内容不进行记录
+	UsedTime           float32                // 处理请求花费的时间，格式为float32，单位毫秒
+	ResponseCode       int                    // 应答代码，200 1000+ 正常应答，201～399，1～199  600～999 特殊应答，<1 异常应答
+	ResponseHeaders    map[string]string      // 应答头，排除掉指定不需要信息后的所有头部内容，敏感数据应脱敏
+	ResponseDataLength uint                   // 应答的数据长度
+	ResponseData       string                 // 指定要记录的数据内容，JSON对象，集合类型仅记录少量内容，敏感数据应脱敏，非对象内容不进行记录
 }
